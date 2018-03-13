979ab566a513fda8b15a141ed151245565b0ad9c
Roberto Decherchi <37223280+rdech@users.noreply.github.com>
2018-03-13 23:05:39 +0100

    Add files via upload
    This is only because i don't remember how to use youtube-dl properly and so i prefer to simplify the command using this script.
    I use my preferred options, so this is not suitable for everyone.

---
+++ ytdl-v1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# this is just a script to simplify the use of youtube-dl.. 
+# Arguments list:
+# -v download video
+# -m download only music
+# -fm or -fv take the link by the file specified as second argument
+
+if [ $1 = -v ]; then
+	youtube-dl -f 'bestvideo+bestaudio' $2
+elif [ $1 = -m ]; then
+	youtube-dl -f 'bestaudio' -x --audio-quality 0 $2
+elif [ $1 = -fm ]; then
+	while read a; do
+		youtube-dl -f 'bestaudio' -x --audio-quality 0 $a
+	done < $2
+elif [ $1 = -fv ]; then
+	while read a; do
+		youtube-dl -f 'bestvideo+bestaudio' $a
+	done < $2
+else
+	echo "error: wrong options.. The avaible options are -v -m -fv -fm"
+fi
+
+exit 0
+

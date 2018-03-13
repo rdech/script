2e3cc6355b6101df8c18459666c2c0287aa17218
Roberto Decherchi <37223280+rdech@users.noreply.github.com>
2018-03-13 23:34:36 +0100

    Update ytdl-v1.sh

--- ytdl-v1.sh
+++ ytdl-v1.sh
@@ -9,10 +9,10 @@
 if [ $1 = -v ]; then
 	youtube-dl -f 'bestvideo+bestaudio' $2
 elif [ $1 = -m ]; then
-	youtube-dl -f 'bestaudio' -x --audio-quality 0 $2
+	youtube-dl -f 'bestaudio' -x --audio-quality 0 --audio-format 'mp3' $2
 elif [ $1 = -fm ]; then
 	while read a; do
-		youtube-dl -f 'bestaudio' -x --audio-quality 0 $a
+		youtube-dl -f 'bestaudio' -x --audio-quality 0 --audio-format 'mp3' $a
 	done < $2
 elif [ $1 = -fv ]; then
 	while read a; do

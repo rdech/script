2bcc3da80afdf3eed9b7dac082fce9744890111b
Roberto Decherchi <37223280+rdech@users.noreply.github.com>
2018-03-15 17:03:00 +0100

    Update ytdl-v1.sh

--- ytdl-v1.sh
+++ ytdl-v1.sh
@@ -6,21 +6,21 @@
 # -m download only music
 # -fm or -fv take the link by the file specified as second argument
 
-if [ $1 = -v ]; then
+if [ "$1"x = -vx ]; then
 	echo " --- Your video will be in $HOME/Videos ---"
 	cd ~/Videos
 	youtube-dl -f 'bestvideo+bestaudio' $2
-elif [ $1 = -m ]; then
+elif [ "$1"x = -mx ]; then
 	echo " --- Your music will be in $HOME/Music ---"
 	cd ~/Music
 	youtube-dl -f 'bestaudio' -x --audio-quality 0 --audio-format 'mp3' $2
-elif [ $1 = -fm ]; then
+elif [ "$1"x = -fmx ]; then
 	echo " --- Your musics will be in $HOME/Music ---"
 	cd ~/Music
 	while read a; do
 		youtube-dl -f 'bestaudio' -x --audio-quality 0 --audio-format 'mp3' $a
 	done < $2
-elif [ $1 = -fv ]; then
+elif [ "$1"x = -fvx ]; then
 	echo " --- Your videos will be in $HOME/Videos ---"
 	cd ~/Videos
 	while read a; do
